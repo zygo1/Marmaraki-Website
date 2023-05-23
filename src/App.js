517e940524d4fece0733dc2101c1934d42dd7c38
zygo1 <dzygolanis@gmail.com>
2023-05-23 22:05:56 +0300

    meteora

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Navigation from './components/Navigation';
 import Help from './components/Help';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Homepage from './components/Homepage';
+import Meteora from './components/Meteora';
 
 function App() {
   return (
@@ -9,7 +11,8 @@ function App() {
       <Router>
         <Navigation />
         <Routes>
-          <Route path='/Meteora' />
+          <Route path='/' element={<Homepage />} />
+          <Route path='/Meteora' element={<Meteora />} />
           <Route path='/Activities' />
           <Route path='/Restaurants' />
           <Route path='/Help' element={<Help />} />
---
+++ src/components/Homepage.jsx
@@ -0,0 +1,12 @@
+
+
+
+const Homepage = () => {
+    return (
+        <>
+
+        </>
+    )
+};
+
+export default Homepage;
